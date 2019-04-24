4153db2236db8e5427bef73691f1b1db0b45b32c
AgathaP <ebrumaktub@gmx.com>
2019-04-24 12:54:17 -0400

    Evento que imprime todos los pokemon en pantalla. main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 /* Manejo del DOM */
+const data = POKEMON.pokemon;
+
+window.onload
+
+
+let home = document.getElementById("root").innerHTML = 
+`
+<button id="enter_page">Entrar</button>
+`;
+// Formato para las cartas que muestran todos los pokemon.
+let pageShowData = document.getElementById("enter_page").addEventListener("click", () => {
+     
+    for(let i = 0; i < data.length; i++){
+        document.getElementById("root").innerHTML +=
+        `
+        <div class="cards-container row">
+            <div id="each-card" class="card col-sm-6 col-2" style="width: 18rem;">
+            <img src="${(data[i].img)}" class="card-img-top" alt="${(data[i].name)}">
+            <h5 class="card-title">${(data[i].name)}</h5>
+            </div>
+        </div>`
+    }  
