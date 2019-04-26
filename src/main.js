1683ea1e2d5d12840577a6c1c029bf415a88169a
AgathaP <ebrumaktub@gmx.com>
2019-04-26 12:56:52 -0400

    asdf

--- src/main.js
+++ src/main.js
@@ -10,14 +10,27 @@ let home = document.getElementById("root").innerHTML =
 `;
 // Formato para las cartas que muestran todos los pokemon.
 let pageShowData = document.getElementById("enter_page").addEventListener("click", () => {
-     
+       
     for(let i = 0; i < data.length; i++){
         document.getElementById("root").innerHTML +=
         `
-        <div class="cards-container row">
-            <div id="each-card" class="card col-sm-6 col-2" style="width: 18rem;">
-            <img src="${(data[i].img)}" class="card-img-top" alt="${(data[i].name)}">
-            <h5 class="card-title">${(data[i].name)}</h5>
-            </div>
-        </div>`
+        <div id="each-card" class="card col-sm-6 col-2" style="width: 18rem;">
+        <img src="${(data[i].img)}" class="card-img-top" alt="${(data[i].name)}">
+        <h5 class="card-title">${(data[i].name)}</h5>
+        </div>
+        `
     }  
+})
+
+    //     // Eventos del modal
+    //     document.getElementById("each-card").addEventListener("click", () => {
+    //         document.getElementById("myModal").style.display="block";
+    //     })
+    
+    //     document.getElementById("close").addEventListener("click", () => {
+    //         document.getElementById("myModal").style.display = "none";
+    //     })
+    //     // cierra el modal al clickear fuera
+    //   document.getElementById("myModal").addEventListener("click", () => {
+    //         document.getElementById("myModal").style.display = "none";
+    //     })
