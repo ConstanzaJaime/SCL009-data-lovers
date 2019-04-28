cd43c22b7fb2ece98b0c927458f57f77c9ce19ab
ConstanzaJaime <constanza.jaime.a@gmail.com>
2019-04-28 11:13:20 -0400

    separacion de pantallas y navbar

--- src/main.js
+++ src/main.js
@@ -2,76 +2,15 @@
 const data = POKEMON.pokemon;
 
 window.onload
+//BLOQUEO DE SEGUNDA PÁGINA AL MOSTRAR LA PRIMERA
+  document.getElementById("root").style.display= "none";
 
-
-let home = document.getElementById("root").innerHTML =
-  `
-<button id="enter_page">Entrar</button>
-`;
-// Formato para las cartas que muestran todos los pokemon.
-let pageShowData = document.getElementById("enter_page").addEventListener("click", () => {
-    document.getElementById("root").innerHTML= 
-   ` <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-  <a class="navbar-brand" href="#">Inicio</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          TIPO
-        </a>
-        <div class="dropdown-menu" id="selectType" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" value="Grass" href="#">Grass</a>
-          <a class="dropdown-item" value="Poison" href="#">Poison</a>
-          <a class="dropdown-item" value="Fire" href="#">Fire</a>
-          <a class="dropdown-item" value="Flying" href="#">Flying</a>
-          <a class="dropdown-item" value="Water" href="#">Water</a>
-          <a class="dropdown-item" value="Bug" href="#">Bug</a>
-          <a class="dropdown-item" value="Normal" href="#">Normal</a>
-          <a class="dropdown-item" value="Electric" href="#">Electric</a>
-          <a class="dropdown-item" value="Ground" href="#">Ground</a>
-          <a class="dropdown-item" value="Fighting" href="#">Fighting</a>
-          <a class="dropdown-item" value="Psychic" href="#">Psychic</a>
-          <a class="dropdown-item" value="Rock" href="#">Rock</a>
-          <a class="dropdown-item" value="Ice" href="#">Ice</a>
-          <a class="dropdown-item" value="Ghost" href="#">Ghost</a>
-          <a class="dropdown-item" value="Dragon" href="#">Dragon</a>
-        </div>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          DEBILIDAD
-        </a>
-        <div class="dropdown-menu" id="selectWeaknesses" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" value="Grass" href="#">Grass</a>
-          <a class="dropdown-item" value="Poison" href="#">Poison</a>
-          <a class="dropdown-item" value="Fire" href="#">Fire</a>
-          <a class="dropdown-item" value="Flying" href="#">Flying</a>
-          <a class="dropdown-item" value="Water" href="#">Water</a>
-          <a class="dropdown-item" value="Bug" href="#">Bug</a>
-          <a class="dropdown-item" value="Electric" href="#">Electric</a>
-          <a class="dropdown-item" value="Ground" href="#">Ground</a>
-          <a class="dropdown-item" value="Fighting" href="#">Fighting</a>
-          <a class="dropdown-item" value="Psychic" href="#">Psychic</a>
-          <a class="dropdown-item" value="Rock" href="#">Rock</a>
-          <a class="dropdown-item" value="Ice" href="#">Ice</a>
-          <a class="dropdown-item" value="Ghost" href="#">Ghost</a>
-          <a class="dropdown-item" value="Dragon" href="#">Dragon</a>
-          <a class="dropdown-item" value="Fairy" href="#">Fairy</a>
-          <a class="dropdown-item" value="Dark" href="#">Dark</a>
-          <a class="dropdown-item" value="Steel" href="#">Steel</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-`
-
-  //TARJETAS CON INFORMACION DEL PERSONAJE
+//BOTON COMENZAR(ENTRA A LA PÁGINA PRINCIPAL Y MUESTRA POKEMON)
+  const enterPage =document.getElementById("enter_page").addEventListener("click", () => {
+  document.getElementById("screen1").style.display= "none";
+  document.getElementById("root").style.display= "block";
+ 
+  //TODAS LAS TARJETAS CON IMAGEN Y NOMBRE DEL PERSONAJE
   for(let i = 0; i < data.length; i++) {
     document.getElementById("root").innerHTML +=
       `
@@ -79,11 +18,27 @@ let pageShowData = document.getElementById("enter_page").addEventListener("click
             <div id="each-card" class="card col-sm-2 col-6" style="width: 18rem;">
             <img src="${(data[i].img)}" class="card-img-top" alt="${(data[i].name)}">
             <h5 class="card-title">${(data[i].name)}</h5>
-            </div>
-        </div>`}
-
+            </div> 
+        </div>`
+  } 
+});
+ //REGRESA A LA PÁGINA PRINCIPAL DONDE SE MUESTRAN TODOS LOS POKEMON
+document.getElementById("showAllPokemon").addEventListener("click", () => {
+      return enterPage;
 });
 
+
+//Formato para las cartas que muestran todos los pokemon.
+//const selectType= document.getElementById("type");
+  /*const selectType= document.getElementById("type");
+
+  selectType.addEventListener("change", () => {//FUNCION BOTON SELECCIONAR TIPO
+
+    let selectTypeValue = document.getElementById("type").value; //TOMAR SELECCION DE USUARIO
+    const typeFilter = data.filter((element)=>{data[element].type.includes(selectTypeValue)}) //FILTRADO DE LO QUE SE VA A IMPRIMIR
+    document.getElementById("root").innerHTML += `hola mundo ${typeFilter}` //UBICACION DE LO QUE SE VA A IMPRIMIR Y CONTENIDO DE LO QUE SE IMPRIME
+  
+  })*/
 //MODAL
 /*
 `<section>
