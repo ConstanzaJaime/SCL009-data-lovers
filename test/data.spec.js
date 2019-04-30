54f946d745d67466d42b375562767790a8d6b04c
AgathaP <ebrumaktub@gmx.com>
2019-04-30 11:46:50 -0400

    eaea Los test de filtro que hicimos en data.spec.js con la fabulosa data en miniatura :3

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,14 +3,44 @@ global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
+const POKEMON = [{
+    "num": "001",
+    "name": "Bulbasaur",
+    "type": "Poison",
+    "weaknesses": "Ice"
+  }, {
+    "num": "003",
+    "name": "Venusaur",
+    "type": "Grass",
+    "weaknesses": 
+      "Fire"
+  }, {
+    "num": "074",
+    "name": "Geodude",
+    "type": "Rock",
+    "weaknesses": "Water"
+  }
+];
 
-describe('example', () => {
+describe('filterType', () => {
   
   it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+    assert.equal(typeof filterType, 'function');
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  it('debería retornar Array Bulbasaur para "Poison" ', () => {
+    assert.deepEqual(window.filterType(POKEMON,'Poison'), [{
+      "num": "001", 
+      "name": "Bulbasaur", 
+      "type": "Poison", 
+    "weaknesses": "Ice"}])
   });
-})
+
+  it('debería retornar Array Geodude para "Water" ', () => {
+    assert.deepEqual(window.filterWeak(POKEMON,'Water'), [{
+      "num": "074",
+      "name": "Geodude",
+      "type": "Rock",
+      "weaknesses": "Water"}]);
+});
+});
